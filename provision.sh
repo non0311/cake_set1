1c2def339b2d10e9f43bdcccbc69d4b94f206374
non0311 <silencesuzuka0311@gmail.com>
2016-10-08 02:34:43 +0900

    provision.shやることをコメントアウトで記載

--- provision.sh
+++ provision.sh
@@ -1,1 +1,21 @@
 #!/bin/sh
+
+#httpd インストール
+
+#mariaDB インストール
+
+#epel,remi インストール
+
+#php関連 インストール
+
+#composer インストール
+
+#composer を使いcakephpライブラリをインストール
+
+#httpd シンボリック作成
+
+#httpd 起動設定
+
+#mariaDB 起動設定
+
+#DB作成(SQL文)
